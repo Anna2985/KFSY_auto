2b3700101516ffc26852244c9e99a49d96159ffa
Anna2985 <hshiting58@gamil.com>
2024-11-01 10:13:28 +0800

    20241101

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
+from Tools.scripts.make_ctype import method
 from pywinauto import Application, timings
-
-#app = Application('win32')
-app = Application('uia')
-#app.start('notepad')
-app.connect(path='UDS022P.exe', timeout=5)
-#app.connect()
+from flask import Flask, jsonify
+app = Flask(__name__)
+uds_app = Application('uia')
+uds_app.connect(path='UDS022P.exe', timeout=5)
 #print(app.windows())
-np = app['程式【UDS022P】版本:1.20.1.1 Butterfly@'] #代表記事本窗體
+np = uds_app['程式【UDS022P】版本:1.20.1.1 Butterfly@'] #代表記事本窗體
 #np.dump_tree()
-#np['查詢資料'].click_input()
-np['匯出'].click_input()
-#print(np.children())
+@app.route('/api/get_file_name', methods=['GET'])
+def get_file_name():
+    # np['查詢資料'].click_input()
+    np['匯出'].click_input()
+    # print(np.children())
+    save_dialog = uds_app.window(title='另存新檔')
+    # save_dialog.dump_tree()
+    file_name_combo = save_dialog.child_window(title='檔案名稱(N):', control_type='ComboBox')
 
-save_dialog = app.window(title='另存新檔')
-#save_dialog.dump_tree()
-file_name_combo = save_dialog.child_window(title='檔案名稱(N):', control_type = 'ComboBox')
+    file_name_edit = file_name_combo.child_window(control_type='Edit')
+    default_file_name = file_name_edit.get_value()
+    save_dialog['存檔(S)'].click_input()
+    check_dialog = uds_app.window(title='程式')
+    check_dialog['OKButton'].click_input()
+    print(f'已取得 fileName : {default_file_name}')
+    return jsonify({
+        'result':default_file_name
+    })
 
-file_name_edit = file_name_combo.child_window(control_type ='Edit')
-default_file_name  = file_name_edit.get_value()
-save_dialog['存檔(S)'].click_input()
-check_dialog = app.window(title='程式')
-check_dialog['OKButton'].click_input()
-print(f'已取得 fileName : {default_file_name}')
+if __name__ == '__main__':
+    app.run(debug=True)
 
-"123"
+
+
