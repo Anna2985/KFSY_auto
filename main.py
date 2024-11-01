84de828b2a5ba99424a0a34510e0376d52f861e9
Anna2985 <hshiting58@gamil.com>
2024-11-01 13:50:34 +0800

    20241101

--- main.py
+++ main.py
@@ -16,34 +16,56 @@ def move_file(source_path, destination_path):
         print(f'檔案已從{source_path}移動到{destination_path}')
     except Exception as e:
         print(f'移動檔案發生錯誤')
-app = Flask(__name__)
-@app.route('/api/get_file_name', methods=['GET'])
-def get_file_name():
-    uds_app = Application('uia')
-    uds_app.connect(path='UDS022P.exe', timeout=5)
-    # print(app.windows())
-    np = uds_app['程式【UDS022P】版本:1.20.1.1 Butterfly@']  # 代表記事本窗體
-    # np.dump_tree()
-    # np['查詢資料'].click_input()
-    np['匯出'].click_input()
-    # print(np.children())
-    save_dialog = uds_app.window(title='另存新檔')
-    # save_dialog.dump_tree()
-    file_name_combo = save_dialog.child_window(title='檔案名稱(N):', control_type='ComboBox')
+uds_app = Application('uia')
+uds_app.connect(path='UDS022P.exe', timeout=5)
+# print(app.windows())
+np = uds_app['程式【UDS022P】版本:1.20.1.1 Butterfly@']  # 代表記事本窗體
+# np.dump_tree()
+# np['查詢資料'].click_input()
+np['匯出'].click_input()
+# print(np.children())
+save_dialog = uds_app.window(title='另存新檔')
+# save_dialog.dump_tree()
+file_name_combo = save_dialog.child_window(title='檔案名稱(N):', control_type='ComboBox')
 
-    file_name_edit = file_name_combo.child_window(control_type='Edit')
-    default_file_name = file_name_edit.get_value()
-    save_dialog['存檔(S)'].click_input()
-    check_dialog = uds_app.window(title='程式')
-    check_dialog['OKButton'].click_input()
-    print(f'已取得 fileName : {default_file_name}')
-    source_file  = fr'C:\{default_file_name}'
-    destination_folder = r"C:\Users\Administrator\Downloads\HIS"
-    #shutil.move(source_file, destination_folder)
-    move_file(source_file,destination_folder)
-    return jsonify({
-        'result':default_file_name
-    })
+file_name_edit = file_name_combo.child_window(control_type='Edit')
+default_file_name = file_name_edit.get_value()
+save_dialog['存檔(S)'].click_input()
+check_dialog = uds_app.window(title='程式')
+check_dialog['OKButton'].click_input()
+print(f'已取得 fileName : {default_file_name}')
+source_file  = fr'C:\{default_file_name}'
+destination_folder = r"C:\Users\Administrator\Downloads\HIS"
+#shutil.move(source_file, destination_folder)
+move_file(source_file,destination_folder)
+# app = Flask(__name__)
+# @app.route('/api/get_file_name', methods=['GET'])
+# def get_file_name():
+#     uds_app = Application('uia')
+#     uds_app.connect(path='UDS022P.exe', timeout=5)
+#     # print(app.windows())
+#     np = uds_app['程式【UDS022P】版本:1.20.1.1 Butterfly@']  # 代表記事本窗體
+#     # np.dump_tree()
+#     # np['查詢資料'].click_input()
+#     np['匯出'].click_input()
+#     # print(np.children())
+#     save_dialog = uds_app.window(title='另存新檔')
+#     # save_dialog.dump_tree()
+#     file_name_combo = save_dialog.child_window(title='檔案名稱(N):', control_type='ComboBox')
+#
+#     file_name_edit = file_name_combo.child_window(control_type='Edit')
+#     default_file_name = file_name_edit.get_value()
+#     save_dialog['存檔(S)'].click_input()
+#     check_dialog = uds_app.window(title='程式')
+#     check_dialog['OKButton'].click_input()
+#     print(f'已取得 fileName : {default_file_name}')
+#     source_file  = fr'C:\{default_file_name}'
+#     destination_folder = r"C:\Users\Administrator\Downloads\HIS"
+#     #shutil.move(source_file, destination_folder)
+#     move_file(source_file,destination_folder)
+#     return jsonify({
+#         'result':default_file_name
+#     })
 
 
 if __name__ == '__main__':
