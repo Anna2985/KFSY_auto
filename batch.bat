8d15add07580e6c83a5a8df35dd20d9db785b404
Anna2985 <hshiting58@gamil.com>
2024-11-26 08:45:15 +0800

    20241126

---
+++ batch.bat
@@ -0,0 +1,5 @@
+@echo off
+chcp 65001 >nul
+cd C:\Users\Administrator\PycharmProjects\KFSY_auto # py file文件路徑
+C:\Users\Administrator\PycharmProjects\KFSY_auto\.venv\Scripts\python.exe main.py
+pause
